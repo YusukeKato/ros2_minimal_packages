03fba8defba5bcdbc1c6a98790b8b05b04ca1bb7
YusukeKato <YusukeKato@users.noreply.github.com>
2025-07-12 20:05:57 +0900

    Add minimal_lifecycle_node (#1)
    * add lifecycle node
    
    * remove __pycache__
    
    * rename package

---
+++ minimal_lifecycle_node/README.md
@@ -0,0 +1,55 @@
+# Minimal Lifecycle Node
+
+## Terminal 1
+
+```sh
+$ ros2 run minimal_lifecycle_node minimal_lifecycle_node
+# [minimal_lifecycle_node_a]: minimal_lifecycle_node_a: initialized.
+# [minimal_lifecycle_node_b]: minimal_lifecycle_node_b: initialized.
+```
+
+## Terminal 2
+
+```sh
+$ ros2 lifecycle set minimal_lifecycle_node_a configure
+# Terminal 1 > "minimal_lifecycle_node_a: on_configure() is called."
+
+$ ros2 lifecycle set minimal_lifecycle_node_a activate
+# Terminal 1 > "minimal_lifecycle_node_a: on_activate() is called."
+
+# --- Start minimal_lifecycle_node_a publisher ---
+
+$ ros2 lifecycle set minimal_lifecycle_node_a deactivate
+# Terminal 1 > "minimal_lifecycle_node_a: on_deactivate() is called."
+
+# --- Stop minimal_lifecycle_node_a publisher ---
+
+$ ros2 lifecycle set minimal_lifecycle_node_b configure
+# Terminal 1 > "minimal_lifecycle_node_b: on_configure() is called."
+
+$ ros2 lifecycle set minimal_lifecycle_node_b activate
+# Terminal 1 > "minimal_lifecycle_node_b: on_activate() is called."
+
+# --- Start minimal_lifecycle_node_b publisher ---
+```
+
+## Terminal 3
+
+```sh
+$ ros2 topic echo /minimal_lifecycle_node_topic
+# data: 'minimal_lifecycle_node_a: 0'
+# ---
+# data: 'minimal_lifecycle_node_a: 1'
+# ---
+# data: 'minimal_lifecycle_node_a: 2'
+# ---
+# ...
+#
+# data: 'minimal_lifecycle_node_b: 0'
+# ---
+# data: 'minimal_lifecycle_node_b: 1'
+# ---
+# data: 'minimal_lifecycle_node_b: 2'
+# ---
+# ...
+```
---
+++ minimal_lifecycle_node/minimal_lifecycle_node/minimal_lifecycle_node.py
@@ -0,0 +1,68 @@
+import rclpy
+
+from rclpy.lifecycle import Node
+from rclpy.lifecycle import State
+from rclpy.lifecycle import TransitionCallbackReturn
+from std_msgs.msg import String
+
+
+class MinimalLifecycleNode(Node):
+    def __init__(self, node_name):
+        super().__init__(node_name)
+        self.node_name: str = node_name
+        self.topic_name: str = 'minimal_lifecycle_node_topic'
+        self.cnt: int = 0
+        self.get_logger().info(self.node_name + ': initialized.')
+
+    def pub_str(self):
+        str_msg = String()
+        str_msg.data = self.node_name + ': ' + str(self.cnt)
+        self.pub.publish(str_msg)
+        self.cnt += 1
+
+    def on_configure(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().info(self.node_name + ': on_configure() is called.')
+        self.pub = self.create_publisher(String, self.topic_name, 10)
+        self.timer = self.create_timer(1.0, self.pub_str)
+        self.timer.cancel()  # stop timer
+        return TransitionCallbackReturn.SUCCESS
+
+    def on_activate(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().info(self.node_name + ': on_activate() is called.')
+        self.timer.reset()  # restart timer
+        return TransitionCallbackReturn.SUCCESS
+
+    def on_deactivate(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().info(self.node_name + ': on_deactivate() is called.')
+        self.timer.cancel()  # stop timer
+        return TransitionCallbackReturn.SUCCESS
+
+    def on_cleanup(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().info(self.node_name + ': on_cleanup() is called.')
+        self.destroy_publisher(self.pub)
+        self.destroy_timer(self.timer)
+        return TransitionCallbackReturn.SUCCESS
+
+    def on_shutdown(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().info(self.node_name + ': on_shutdown() is called.')
+        self.destroy_publisher(self.pub)
+        self.destroy_timer(self.timer)
+        return TransitionCallbackReturn.SUCCESS
+
+
+def main():
+    rclpy.init()
+    executor = rclpy.executors.SingleThreadedExecutor()
+    minimal_lifecycle_node_a = MinimalLifecycleNode('minimal_lifecycle_node_a')
+    minimal_lifecycle_node_b = MinimalLifecycleNode('minimal_lifecycle_node_b')
+    executor.add_node(minimal_lifecycle_node_a)
+    executor.add_node(minimal_lifecycle_node_b)
+    try:
+        executor.spin()
+    except (KeyboardInterrupt, rclpy.executors.ExternalShutdownException):
+        minimal_lifecycle_node_a.destroy_node()
+        minimal_lifecycle_node_b.destroy_node()
+
+
+if __name__ == '__main__':
+    main()
